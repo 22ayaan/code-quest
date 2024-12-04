f30d20a8ecfdcc60219e82e032aa592d4c4ba3bb
Ayaan Siddiqui <ars394@pitt.edu>
2024-12-04 18:43:19 -0500

    add problemsets to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,33 +1,48 @@
 import React from "react";
 import Header from "./Header";
 import { Link } from "react-router-dom";
+import { questions } from "../constants/questions";
 
 const Dashboard = () => {
   return (
     <div>
       <Header />
       <div className="w-full h-screen p-4 mx-auto bg-stone-800 grow">
-        <div className="pt-4">
-          <p className="text-[20px] font-medium text-gray-300 mb-8">Problemset</p>
-          <div className="grid grid-cols-3 gap-4">
-            <Link to="/problems/two-sum">
-              <div className="relative flex items-start p-2 duration-300 bg-white rounded-lg cursor-pointer"></div>
-            </Link>
-            <Link to="/problems/two-sum">
-              <div className="relative flex items-start p-2 duration-300 bg-white rounded-lg cursor-pointer"></div>
-            </Link>
-            <Link to="/problems/two-sum">
-              <div className="relative flex items-start p-2 duration-300 bg-white rounded-lg cursor-pointer"></div>
-            </Link>
-            <Link to="/problems/two-sum">
-              <div className="relative flex items-start p-2 duration-300 bg-white rounded-lg cursor-pointer"></div>
-            </Link>
-            <Link to="/problems/two-sum">
-              <div className="relative flex items-start p-2 duration-300 bg-white rounded-lg cursor-pointer"></div>
-            </Link>
-            <Link to="/problems/two-sum">
-              <div className="relative flex items-start p-2 duration-300 bg-white rounded-lg cursor-pointer"></div>
-            </Link>
+        <div className="mx-auto w-full grow p-4 md:max-w-[888px] md:p-6 lg:max-w-screen-xl">
+          <div>
+            <p className="text-[20px] font-medium text-gray-300 mb-8">Problemset</p>
+            <div className="grid grid-cols-3 gap-4">
+              {questions.map((item) => (
+                <Link to="/problems/two-sum">
+                  <div
+                    key={item}
+                    className="relative flex items-start px-3 py-2 duration-300 rounded-lg cursor-pointer thumbnail-box-bg"
+                  >
+                    <img
+                      src="./thumbnail.png"
+                      alt="thumbnail"
+                      className="rounded-[4px] h-[72px] w-[72px] mr-3.5 my-auto"
+                    />
+                    <div className="flex-grow py-1">
+                      <div className="flex flex-col justify-center h-full">
+                        <div className="flex items-start">
+                          <p className="font-medium mb-1.5 text-lg line-clamp-1">
+                            {item?.title}
+                          </p>
+                        </div>
+                        <div className="mb-1 text-sm text-gray-400">
+                          Difficulty:{" "}
+                          <span className={item?.style}>{item?.diff}</span>
+                        </div>
+                        <div className="line-clamp-2">
+                          <p className="text-[10px] text-gray-400">{item?.desc}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- src/constants/questions.js
+++ src/constants/questions.js
@@ -28,3 +28,48 @@ export const javascriptDefault = `/**
 // console.log(binarySearch(arr, target));
 console.log("helloworld");
 `;
+
+export const questions = [
+  {
+    id: 1,
+    title: "Two Sum",
+    diff: "Easy",
+    style: "!text-teal-400 font-bold",
+    desc: "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.",
+  },
+  {
+    id: 2,
+    title: "Longest Substring Without Repeating Characters",
+    diff: "Medium",
+    style: "!text-green-400 font-bold",
+    desc: "Given a string s, find the length of the longest substring without repeating characters.",
+  },
+  {
+    id: 3,
+    title: "Merge Intervals",
+    diff: "Medium",
+    style: "!text-green-400 font-bold",
+    desc: "Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.",
+  },
+  {
+    id: 4,
+    title: "Find Median from Data Stream",
+    diff: "Hard",
+    style: "!text-red-500 font-bold",
+    desc: "The MedianFinder class has two methods: addNum(int num) adds the integer num from the data stream and findMedian() returns the median of all elements so far.",
+  },
+  {
+    id: 5,
+    title: "Search in Rotated Sorted Array",
+    diff: "Medium",
+    style: "!text-green-400 font-bold",
+    desc: "There is an integer array nums sorted in ascending order (with distinct values). Prior to being passed to your function, nums is rotated at an unknown pivot index. Given the rotated array nums, search for a given target.",
+  },
+  {
+    id: 6,
+    title: "Reverse Nodes in k-Group",
+    diff: "Hard",
+    style: "!text-red-500 font-bold",
+    desc: "Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list. k is a positive integer less than or equal to the length of the linked list.",
+  },
+];
